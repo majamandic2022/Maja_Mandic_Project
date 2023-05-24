131adc15e94faa4a201eb67a90b8e3d2da4ee0a5
maja <maja.miljic@nimbus-tech.io>
2023-05-24 21:41:49 +0200

    init of project with prettier,pre-commit, tsconfig and translation setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import "./App.scss"
+
+import reactLogo from './assets/react.svg';
+
+import viteLogo from '/vite.svg';
+
+import './app.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const { t } = useTranslation();
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className='logo' alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
@@ -20,20 +23,16 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <h1>{t('greeting')}</h1>
         <p>{t('farewell')}</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/app.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from './App.tsx'
-import './i18n'
+import App from './app.tsx';
+import './i18n';
 
-import './index.scss'
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
